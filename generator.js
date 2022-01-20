81b4512784f7451208628fd499c03e03dd5083d6
username <nomadicalli@mail.ru>
2022-01-20 11:04:51 +0300

    release 2

--- generator.js
+++ generator.js
@@ -1,8 +1,12 @@
-function passwordGenerator() {
+function passwordGenerator(number) {
     const letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    const array = letters.split('');
+    if (number < 5 || isNaN(number)) {
+        console.log('Ошибка')
+    } else {
+    const array = letters.split('', number);
     array.sort(() => Math.random() - 0.5);
+    
     return array.join('');
-
+    }
 
 }
