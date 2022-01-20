a61a9a465ecc00769fff9e8d4f344c4387ee8621
username <nomadicalli@mail.ru>
2022-01-20 12:32:16 +0300

    release 3

--- generator.js
+++ generator.js
@@ -1,12 +1,25 @@
-function passwordGenerator(number) {
-    const letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    if (number < 5 || isNaN(number)) {
-        console.log('Ошибка')
-    } else {
-    const array = letters.split('', number);
-    array.sort(() => Math.random() - 0.5);
+function passwordGenerator(number, complexity) {
+    let letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let num = '0123456789';
+    let symbol = '!#$%&()*+,-./:;<=>?@[\]^_{|}~';
     
-    return array.join('');
+    if (complexity === 'easy') {
+        letters;
+    } else if (complexity === 'medium') {
+    letters = num + letters;
+    } else if (complexity === 'hard') {
+    letters = symbol + letters + num;
     }
 
+    if (number < 5 || isNaN(number)) {
+        console.log('Ошибка'); 
+    } else {
+      
+        const array = letters.split('', number);
+        array.sort(() => Math.random() - 0.5);
+        return array.join('');
+    }
 }
+    
+    
+    
