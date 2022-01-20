328566fb78d8c8b001bdeea8da927169b26cbecf
username <nomadicalli@mail.ru>
2022-01-20 15:57:59 +0300

    release 4

--- generator.js
+++ generator.js
@@ -1,24 +1,39 @@
-function passwordGenerator(number, complexity) {
+function passwordGenerator(number, complexity, count = 1) {
     let letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     let num = '0123456789';
     let symbol = '!#$%&()*+,-./:;<=>?@[\]^_{|}~';
+    let passwordArr = [];
+
     
     if (complexity === 'easy') {
         letters;
+
     } else if (complexity === 'medium') {
-    letters = num + letters;
+        letters = num + letters;
+        
     } else if (complexity === 'hard') {
-    letters = symbol + letters + num;
+        letters = symbol + letters + num;
     }
 
-    if (number < 5 || isNaN(number)) {
+    if (number < 5 || isNaN(number) || count < 1 || isNaN(count)) {
         console.log('Ошибка'); 
+    
     } else {
       
-        const array = letters.split('', number);
+        let array = letters.split('');
+        
+       for (let i = 0; i < count; i++) {
+       
         array.sort(() => Math.random() - 0.5);
-        return array.join('');
+
+        passwordArr.push(array.join('').substr(0, number));
+      }
     }
+    const uniqueSet = new Set(passwordArr);
+
+    const newPasswords = [...uniqueSet];
+
+    return newPasswords;
 }
     
     
